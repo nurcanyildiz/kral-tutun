eed6f2a0483bfe99b3679a484c937155211b5d96
“Your <you@example.com>
2024-02-18 13:37:27 +0100

    first commit

---
+++ src/contact.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./componant/navbar";
+import React from "react";
+
+function Contact(){
+    return (
+        <>
+            <Navbar/>
+            <h1>Contact me from here</h1>
+        </>
+
+    )
+}
+export default Contact
---
+++ src/product.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./componant/navbar";
+import React from "react";
+
+function Product(){
+    return (
+        <>
+            <Navbar/>
+            <h1>Our Product list</h1>
+        </>
+
+    )
+}
+export default Product
